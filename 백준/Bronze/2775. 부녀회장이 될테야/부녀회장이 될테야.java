7d0c163d2aa9d1e6a7fe302a66c23df6164a47b1
yuhyejin <74812194+yuhyejin@users.noreply.github.com>
2024-07-13 01:39:32 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 100 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -11,17 +11,22 @@ public class Main {
         for (int i = 0; i < t; i++) {
             int k = Integer.parseInt(br.readLine());
             int n = Integer.parseInt(br.readLine());
+            
             int[][] arr = new int[k+1][n+1];
 
+            // 0층 초기화 1~n
             for (int j = 1; j <=n; j++) {
                 arr[0][j] = j;
             }
 
+            // k층의 n호부터 
             for (int j = 1; j <= k; j++) {
             	for (int p = 1; p <= n; p++) {
+                    // 1호는 항상 1
             		if(p == 1)
             			arr[j][p] = 1;
             		else {
+                        // [1][1] = [0][1] + [1][0] 1층 1호 구하기
             			arr[j][p] = arr[j-1][p] + arr[j][p-1]; 
             		}
             	}
