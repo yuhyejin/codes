e82f015e950e2909832c5bf5f1759488cf12f546
yuhyejin <74812194+yuhyejin@users.noreply.github.com>
2024-10-07 00:29:25 +0900

    [Gold IV] Title: DSLR, Time: 2840 ms, Memory: 304224 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -7,7 +7,6 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int T, A, B;
-	static boolean[] visited;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
