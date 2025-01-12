78fd39a5139170d77887a7895f6b2b59857ac1e4
yuhyejin <74812194+yuhyejin@users.noreply.github.com>
2025-01-12 18:35:05 +0900

    [Silver IV] Title: 수학숙제, Time: 108 ms, Memory: 36944 KB -BaekjoonHub

--- 백준/Silver/2870. 수학숙제/수학숙제.py
+++ 백준/Silver/2870. 수학숙제/수학숙제.py
@@ -7,19 +7,8 @@ result = []
 
 for i in arr:
     result.extend(re.findall(r'\d+', i))
-    
-new_result = []
-for i in result:
-    if len(i) > 1:
-        for j in range(len(i)):
-            if i[j] != '0':
-                i = i[j:]
-                break
-        new_result.append(i)
-    else:
-        new_result.append(i)
 
-new_result = sorted(map(int, new_result))
+new_result = sorted(map(int, result))
 
 for i in new_result:
     print(i)
