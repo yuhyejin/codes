dd6785e3ffef5427f24f3689ab68e2b3890c2579
yuhyejin <74812194+yuhyejin@users.noreply.github.com>
2025-02-22 02:51:57 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -3,7 +3,6 @@ from collections import deque
 input = sys.stdin.readline
 n, m = map(int, input().split())
 graph = [list(map(int, input().rstrip())) for _ in range(n)]
-dist = [[-1] * m for _ in range(n)]
 
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
@@ -11,7 +10,6 @@ dy = [1, -1, 0, 0]
 def bfs(x, y):
     queue = deque()
     queue.append((x, y))
-    dist[x][y] = 0  # 시작점 거리 초기화
     
     while queue:
         x, y = queue.popleft()
@@ -19,9 +17,9 @@ def bfs(x, y):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < n and 0 <= ny < m:
-                if graph[nx][ny] == 1 and dist[nx][ny] == -1:
+                if graph[nx][ny] == 1:
+                    graph[nx][ny] = graph[x][y] + 1
                     queue.append((nx, ny))
-                    dist[nx][ny] = dist[x][y] + 1
-    return dist[n-1][m-1]+1
+    return graph[n-1][m-1]
 
 print(bfs(0, 0))
