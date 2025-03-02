6cdf10aea5889247fea0d0e46d9f4f528bce26b6
yuhyejin <74812194+yuhyejin@users.noreply.github.com>
2025-03-03 01:36:38 +0900

    [Silver I] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -3,13 +3,4 @@ input = sys.stdin.readline
 
 a, b, c = map(int, input().split())
 
-def func(a, b, c):
-    if b == 1:
-        return a % c
-    val = func(a, b // 2, c)
-    if b % 2 == 0:
-        return val * val % c
-    else :
-        return val * val * a % c
-    
-print(func(a, b, c))
+print(pow(a, b, c))
